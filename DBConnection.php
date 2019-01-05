d6f714443d3db65950d3123d813abadde2479801
Yining He <1191401005@qq.com>
2019-01-05 21:40:07 +0100

    for frameworkasfs

--- DBConnection.php
+++ DBConnection.php
@@ -30,19 +30,23 @@ class DBConnection{
     }
 
 
-    public function saveUserMelody($userid, $melody){
+    public function saveUserMelody($userid, $melody)
+    {
+        if ($this->db) {
+            $notelist = $melody->getNoteList();
+            $timelist = $melody->getTimeList();
+            $notesize = count($notelist);
+            if ($this->db) {
+                $sql = "insert into usermelody values('" . $userid . "')";
+                $result = $this->db->query($sql);
+                $melodyid = mysqli_insert_id();
+                for ($i = 0; $i < $notesize; $i++) {
+                    $note = $notelist[$i];
+                    $time = $timelist[$i];
+                    $sql2 = "insert into melody (note,length,melodyid) values('".$note."','".$time."','".$melodyid."')";
+                    $this->db->query($sql2);
 
-        $notelist = $melody->getNoteList();
-        $timelist = $melody->getTimeList();
-        $notesize = count($notelist);
-        if($this->db) {
-            $sql = "insert into usermelody values('".$userid."')";
-            $result = $this->db->query($sql);
-            $melodyid = mysqli_insert_id();
-            for ($i = 0; $i < $notesize; $i++) {
-                $note = $notelist[$i];
-                $time = $timelist[$i];
-                $sql2 = "insert into ";
+                }
             }
         }
     }
@@ -51,11 +55,20 @@ class DBConnection{
 
     }
 
-    public function saveMelody($melody){
+    public function getUserMelody($userid){
+        if($this->db) {
+            $sql = "select * from usermelody where userid = '".$userid."'";
+            $result = $this->db->query($sql);
+
+        }
 
     }
 
-    public function validateUser(){
+    public function checkUser($userpassword){
+        if($this->db){
+            $sql = "select * from user where username ='".$userpassword."'";
+
+        }
     }
 
     public function closeDataBase(){
