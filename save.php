92b3dcc1bdd776d61ff3fd572faebb0cef4402ea
Yining He <1191401005@qq.com>
2019-01-09 18:31:41 +0100

    for save php

--- save.php
+++ save.php
@@ -5,15 +5,27 @@
  * Date: 2019/1/5
  * Time: 23:34
  */
-include "Main_Class.php" ;
+
 include "DBConnection.php";
 function validateInput(){
     return true ;
 }
 if(validateInput()) {
-        $DBobject = new DBConnection();
+
         $melody = new Melody();
+        $score = new Score();
         $userid = "";
+        var_dump($_POST) ;
+        $chordlist = $_POST["chordlist"];
+        $timelist = $_POST["timelist"];
+        $notelist = $_POST["notelist"];
+        $lengthlist = $_POST["lengthlist"];
+        $chordlist = json_decode($chordlist);
+        $count = count($chordlist);
+        for($i=0;$i<$count;$i++){
+            echo $chordlist[$i]."</br>";
+        }
+        $DBobject = new DBConnection();
         $DBobject->saveUserMelody($userid , $melody);
         $DBobject->closeDataBase();
 
